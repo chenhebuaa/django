ed79a49816b29775dc939c325bf479dec34f9a72
chenhebuaa <chenhebuaa@gmail.com>
2017-08-17 16:40:25 +0800

    Add filter.sh

---
+++ filter.sh
@@ -0,0 +1,15 @@
+git filter-branch --env-filter '
+OLD_EMAIL="chenhe@qiyi.com"
+CORRECT_NAME="chenhebuaa"
+CORRECT_EMAIL="chenhebuaa@gmail.com"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
